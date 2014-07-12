0cc6260d5b9c86c4bbbcfaa1cb996c08975a80e5
IvarsZ <Ivars.Zubkans@gmail.com>
2014-07-12 16:14:45 +0100

    Add api getter for all accidents

--- app/controllers/accidents_controller.rb
+++ app/controllers/accidents_controller.rb
@@ -6,16 +6,22 @@ class AccidentsController < ApplicationController
       @accident = Accident.new(accident_params)
       if @accident.save
         format.json {render json: {success: true}}    
+      else
+        format.json {render json: {success: false}}
       end
     end
   end 
 
+  def all_accidents
+    respond_to do |format|
+      format.json {render json: Accident.all.to_json }
+    end
+  end
+
   private
     
     def accident_params
-      puts "TEST"
       puts params
-      puts "END TEST"
       params.require(:accident).permit(:time, :severity, :vehicle, :lat, :lng)
     end
 end
