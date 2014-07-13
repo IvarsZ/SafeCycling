7efd9013ea86062bca871f391f6bb5b60c41ff39
IvarsZ <Ivars.Zubkans@gmail.com>
2014-07-13 15:20:52 +0100

    Limit accident submissions per hour

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711103517) do
+ActiveRecord::Schema.define(version: 20140712174254) do
 
   create_table "accidents", force: true do |t|
     t.datetime "time"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20140711103517) do
     t.string   "vehicle"
     t.float    "lat"
     t.float    "lng"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ips", id: false, force: true do |t|
+    t.string   "ip"
+    t.integer  "times_visited"
+    t.integer  "accidents_submitted"
+    t.integer  "accidents_in_last_hour"
+    t.datetime "hourly_accidents_timer"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
